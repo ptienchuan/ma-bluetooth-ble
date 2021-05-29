f8556a700fcb2b2f555a17539eef87b79d615723
Phạm Tiến Chuẩn <ptienchuan@gmail.com>
2021-05-29 16:23:03 +0700

    scan devices

--- App.tsx
+++ App.tsx
@@ -1,12 +1,61 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button, SafeAreaView } from "react-native";
+import { BleManager } from "react-native-ble-plx";
+
+const bleManager = new BleManager();
 
 export default function App() {
+  const [isScanning, setIsScanning] = useState(false);
+  const [devices, setDevices] = useState<string[]>([]);
+
+  const startScanning = () => {
+    setIsScanning(true);
+    bleManager.startDeviceScan(
+      null,
+      { allowDuplicates: false },
+      (error, scannedDevice) => {
+        if (error) {
+          stopScanning();
+          return console.log(error);
+        }
+        if (scannedDevice) {
+          const { id, name } = scannedDevice;
+          console.log({ id, name });
+          if (name) {
+            setDevices(devices.concat([name]));
+          }
+        }
+      }
+    );
+  };
+
+  const stopScanning = () => {
+    setIsScanning(false);
+    setDevices([]);
+    bleManager.stopDeviceScan();
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <Text>New line</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.functions}>
+        {isScanning ? (
+          <>
+            <Text>Scanning ...</Text>
+            <Button title="Stop scanning" onPress={stopScanning} />
+          </>
+        ) : (
+          <Button title="Scan" onPress={startScanning} />
+        )}
+      </View>
+      <View style={styles.devices}>
+        <Text style={styles.title}>LIST DEVICES</Text>
+        {devices.map((deviceName, index) => (
+          <Text style={styles.device} key={index}>
+            {deviceName}
+          </Text>
+        ))}
+      </View>
+    </SafeAreaView>
   );
 }
 
@@ -15,6 +64,24 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
-    justifyContent: "center",
+  },
+  functions: {
+    width: "100%",
+    paddingVertical: 20,
+    alignItems: "center",
+    backgroundColor: "#DAE8FC",
+  },
+  devices: {
+    width: "100%",
+    paddingVertical: 20,
+    alignItems: "center",
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 18,
+    marginVertical: 10,
+  },
+  device: {
+    marginVertical: 5,
   },
 });
